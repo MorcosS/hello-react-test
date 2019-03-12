780e77e8a54e2fd17367373d31316df8b76cc17b
MorcosS <=>
2019-03-12 14:08:55 +0200

    updated app

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React Yay Yahoo!
+            Hello Auto builds!
           </a>
         </header>
       </div>

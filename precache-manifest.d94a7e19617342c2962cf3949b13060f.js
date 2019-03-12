56ff12034c3e70a5456df6bae2d9385362e4e192
MorcosS (from Travis CI) <morkos.said@gmail.com>
2019-03-12 12:11:25 +0000

    Deploy MorcosS/hello-react-test to github.com/MorcosS/hello-react-test.git:gh-pages

---
+++ precache-manifest.d94a7e19617342c2962cf3949b13060f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "46f87c4c45bee020cf79",
+    "url": "/hello-react-test/static/css/main.1e405bee.chunk.css"
+  },
+  {
+    "revision": "46f87c4c45bee020cf79",
+    "url": "/hello-react-test/static/js/main.3a695b0a.chunk.js"
+  },
+  {
+    "revision": "1416dcc56d96d18e8de4",
+    "url": "/hello-react-test/static/js/runtime~main.facca1b2.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/hello-react-test/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello-react-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "88bf9d98c3b8f00f2cdd7d66fce535cc",
+    "url": "/hello-react-test/index.html"
+  }
+];
